fcf370166296bdbd5b95ba1d95bbfaa2b2633173
Joseph Pirtle <JosephP@gust.com>
2014-11-21 16:24:32 -0500

    it works

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,25 @@
-import Data.List (isPrefixOf, sort, intersperse)
+import Data.List (isPrefixOf, isInfixOf, sort, intersperse)
 import Text.Regex.Posix
+import qualified System.IO.Strict as S
 
 data Version = Version {unVersion :: String} deriving (Eq, Ord, Show)
 
+schemaPrefix = "INSERT INTO schema_migrations (version) VALUES ('"
+schemaSuffix = "');"
+conflictMarker = "<<<"
+migrationVersionPattern = "[0-9]+"
+structureFilename = "db/structure.sql"
 
 main :: IO ()
 main = do
-  s <- readFile "db/structure.sql" 
-  writeFile "db/resolved_structure.sql" $ resolve s
+  s <- S.readFile structureFilename
+  case resolve s of
+    Just rs ->
+      writeFile structureFilename rs
+    Nothing -> 
+      putStrLn "Merge conflicts found in structure that cannot be auto-resolved, please resolve manually"
   where 
-    resolve :: String -> String
+    resolve :: String -> Maybe String
     resolve s = 
       let
         structurePrefix :: String -> [String]
@@ -17,23 +27,25 @@ main = do
 
         resolveVersions :: String -> [String]
         resolveVersions = addSpacing . map writeVersion . sort . uniqVersions . map readVersion . filterJunk . lines
-      in
-        unlines $ (structurePrefix s) ++ (resolveVersions s)
 
-    schemaPrefix = "INSERT INTO schema_migrations (version) VALUES ('"
-    schemaSuffix = "');"
+        mergeConflictInSchema :: [String] -> Bool
+        mergeConflictInSchema = any (conflictMarker `isInfixOf`)
+      in
+        if mergeConflictInSchema $ structurePrefix s
+          then Nothing
+          else Just $ unlines $ (structurePrefix s) ++ (resolveVersions s)
 
     filterJunk :: [String] -> [String]
     filterJunk = filter $ isPrefixOf schemaPrefix
 
     readVersion :: String -> Version
-    readVersion l = Version $ l =~ "[0-9]+"
+    readVersion l = Version $ l =~ migrationVersionPattern
 
     uniqVersions :: Eq a => [a] -> [a]
-    uniqVersions vs = foldl (\acc v -> if v `elem` acc then acc else v:acc) [] vs
+    uniqVersions = foldl (\acc v -> if v `elem` acc then acc else v:acc) []
 
     addSpacing :: [String] -> [String]
-    addSpacing ss = intersperse "" ss
+    addSpacing = intersperse ""
 
     writeVersion :: Version -> String
     writeVersion v = schemaPrefix ++ unVersion v ++ schemaSuffix
